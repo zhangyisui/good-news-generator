955d2226ee55979a9ff80440798f5ffa897f5a3e
zhangyisui <hechugetqiufrost@outlook.com>
2025-02-05 19:39:12 +0800

    样式修改

--- script.js
+++ script.js
@@ -1,105 +1,117 @@
 let picBase64 = null;
 
 function createNews(type, context) {
-    let fontSizePercent = document.getElementById("font-size").value;  // 获取字体大小的值（百分比）
-    
-    // 将百分比转换为 40px 到 200px 之间的字体大小
-    let fontSize = 40 + (fontSizePercent / 500) * (200 - 40);  // 百分比值转换为 40px 到 200px 之间
+  let fontSizePercent = document.getElementById("font-size").value; // 获取字体大小的值（百分比）
 
-    let img = document.getElementById("main_img");
-    img.innerHTML = `
+  let fontSize = 1 + (fontSizePercent / 500) * (120 - 1); // 字号按百分比转换为 1px 到 120px 之间
+
+  let img = document.getElementById("main_img");
+  img.innerHTML = `
         <img class="bg" src="./img/${type}.webp" />
-        <p class="context" style="font-size: ${fontSize}px; ${type == 'good_news' ? 
-            'color: #dc3023; text-shadow: 0 0 5px rgba(255,240,0,0.8), 0 0 10px rgba(255,240,0,0.8), 0 0 15px rgba(255,240,0,0.8);' : 
-            'color: #5a5a5a;'}">
+        <p class="context" style="font-size: ${fontSize}px; ${
+    type == "good_news"
+      ? "color: #dc3023; text-shadow: 0 0 5px rgba(255,240,0,0.8), 0 0 10px rgba(255,240,0,0.8), 0 0 15px rgba(255,240,0,0.8);"
+      : "color: #5a5a5a;"
+  }">
             ${context}
         </p>
     `;
-};
+}
 
 function createNewsByImage(type, contextBase64) {
-    let img = document.getElementById("main_img");
-    img.innerHTML = `
+  let img = document.getElementById("main_img");
+  img.innerHTML = `
         <img class="bg" src="./img/${type}.webp" />
-        <img class="ft" src="${contextBase64}" ${type == "bad_news" ? 'style="filter: grayscale(1);"' : ""} />
+        <img class="ft" src="${contextBase64}" ${
+    type == "bad_news" ? 'style="filter: grayscale(1);"' : ""
+  } />
     `;
-};
+}
 
 function getNewsType() {
-    if (document.getElementById("good").checked) {
-        return "good_news";
-    }
-    else if (document.getElementById("bad").checked) {
-        return "bad_news";
-    }
-    return "unknown";
-};
+  if (document.getElementById("good").checked) {
+    return "good_news";
+  } else if (document.getElementById("bad").checked) {
+    return "bad_news";
+  }
+  return "unknown";
+}
 
 window.addEventListener("load", () => {
-    createNews("good_news", "你有什么事?");
+  createNews("good_news", "你有什么事?");
 
-    document.getElementById("main_make").addEventListener("click", () => {
-        picBase64 = null;
-        createNews(getNewsType(), document.getElementById("main_context").value);
+  document.getElementById("main_make").addEventListener("click", () => {
+    picBase64 = null;
+    createNews(getNewsType(), document.getElementById("main_context").value);
+  });
+
+  document.getElementById("good").onchange = document.getElementById(
+    "bad"
+  ).onchange = () => {
+    if (picBase64 === null) {
+      createNews(getNewsType(), document.getElementById("main_context").value);
+    } else {
+      createNewsByImage(getNewsType(), picBase64);
+    }
+  };
+
+  document.getElementById("main_makepic").addEventListener("click", () => {
+    document.getElementById("picSelector").click();
+  });
+
+  document
+    .getElementById("picSelector")
+    .addEventListener("change", function () {
+      let reader = new FileReader();
+      reader.readAsDataURL(this.files[0]);
+      reader.addEventListener("load", () => {
+        picBase64 = reader.result;
+        createNewsByImage(getNewsType(), picBase64);
+      });
+      this.value = null;
     });
 
-    document.getElementById("good").onchange = document.getElementById("bad").onchange = () => {
-        if (picBase64 === null) {
-            createNews(getNewsType(), document.getElementById("main_context").value);
-        }
-        else {
-            createNewsByImage(getNewsType(), picBase64);
-        }
-    };
+  document.getElementById("font-size").addEventListener("input", () => {
+    let fontSizePercent = document.getElementById("font-size").value;
+    createNews(getNewsType(), document.getElementById("main_context").value);
+  });
 
-    document.getElementById("main_makepic").addEventListener("click", () => {
-        document.getElementById("picSelector").click();
+  document.getElementById("main_copy").addEventListener("click", () => {
+    domtoimage.toBlob(document.getElementById("main_img")).then((blob) => {
+      navigator.clipboard.write([
+        new ClipboardItem({
+          "image/png": blob,
+        }),
+      ]);
+      setTimeout(() => {
+        alert("复制成功！");
+      }, 200);
     });
+  });
 
-    document.getElementById("picSelector").addEventListener("change", function () {
-        let reader = new FileReader();
-        reader.readAsDataURL(this.files[0]);
-        reader.addEventListener("load", () => {
-            picBase64 = reader.result;
-            createNewsByImage(getNewsType(), picBase64);
+  document.getElementById("main_save").addEventListener("click", () => {
+    domtoimage.toJpeg(document.getElementById("main_img")).then((url) => {
+      let a = document.createElement("a");
+      a.href = url;
+      a.download = `${getNewsType()}.jpg`;
+      a.click();
+    });
+  });
+
+  document.getElementById("main_share").addEventListener("click", () => {
+    domtoimage.toBlob(document.getElementById("main_img")).then((blob) => {
+      navigator
+        .share({
+          files: [
+            new File([blob], `${getNewsType()}.png`, { type: "image/png" }),
+          ],
+        })
+        .then(() => {
+          alert("分享成功！");
+        })
+        .catch(() => {
+          alert("分享失败！");
         });
-        this.value = null;
     });
-
-    document.getElementById("font-size").addEventListener("input", () => {
-        let fontSizePercent = document.getElementById("font-size").value;
-        createNews(getNewsType(), document.getElementById("main_context").value);
-    });
-
-    document.getElementById("main_copy").addEventListener("click", () => {
-        domtoimage.toBlob(document.getElementById("main_img")).then((blob) => {
-            navigator.clipboard.write([new ClipboardItem({
-                "image/png": blob
-            })]);
-            setTimeout(() => {
-                alert("复制成功！");
-            }, 200);
-        });
-    });
-
-    document.getElementById("main_save").addEventListener("click", () => {
-        domtoimage.toJpeg(document.getElementById("main_img")).then((url) => {
-            let a = document.createElement("a");
-            a.href = url;
-            a.download = `${getNewsType()}.jpg`;
-            a.click();
-        });
-    });
-
-    document.getElementById("main_share").addEventListener("click", () => {
-        domtoimage.toBlob(document.getElementById("main_img")).then((blob) => {
-            navigator.share({
-                files: [new File([blob], `${getNewsType()}.png`, { type: "image/png" })]
-            }).then(() => {
-                alert("分享成功！");
-            }).catch(() => {
-                alert("分享失败！");
-            });
-        });
-    });
+  });
 });
